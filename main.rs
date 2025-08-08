893a927ba68c76cfa7a414c02b46e8f31e41f282
yuxxeun <itsyuxxeun@gmail.com>
2025-08-08 11:13:40 +0700

    main.rs

---
+++ main.rs
@@ -0,0 +1,17 @@
+pub const DC: [&str; 10] = [
+    "9sn1jdtq", "eapgbkhe", "09oc1lgd", "23nghr0t", "6izsllrn", "i2oirxah", "yeqsx49h", "i8xaeohc",
+    "vkjs5fme", "60y2usvr",
+];
+
+pub const RX: [&str; 10] = [
+    "zoqxjvuwk",
+    "xyksqdnf4",
+    "1zi3akx6t",
+    "ntz1hid7g",
+    "4dqqdt4ee",
+    "rxfchavlw",
+    "lvjym6589",
+    "wd3w12q5h",
+    "bpmsit1eo",
+    "b3mfx34fj",
+];
